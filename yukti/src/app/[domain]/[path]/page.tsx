e92aef32e406b29049e5c6e996f90193a5f27f11
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-01-22 23:22:37 +0530

    3rd commit

--- yukti/src/app/[domain]/[path]/page.tsx
+++ yukti/src/app/[domain]/[path]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>123</div>
+    <div>chandadksadlskajdkla</div>
   )
 }
 
--- yukti/src/middleware.ts
+++ yukti/src/middleware.ts
@@ -1,13 +1,27 @@
-import { NextResponse } from 'next/server';
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/site(.*)', '/api/uploadthing(.*)'])<sup><a href="#">1</a></sup> 
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth()
+
+  const url = req.nextUrl
+  const searchParams = url.searchParams.toString()
+  let hostname = req.headers
+
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ''
+  }`
+
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
-};
+}
