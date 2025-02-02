bda6f0d5eeb3be375b6053fba5f5e71f3c040384
Chandan Kumar Yadav <chandanydv0402@gmail.com>
2025-02-02 23:33:55 +0530

    queries done

--- yukti/src/middleware.ts
+++ yukti/src/middleware.ts
@@ -1,27 +1,51 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
+import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/site(.*)', '/api/uploadthing(.*)'])<sup><a href="#">1</a></sup> 
-
+const isPublicRoutes = createRouteMatcher(["/site", "/site(.*)", "/agency", "/agency(.*)"]);
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth()
+  if (!isPublicRoutes(req)) await auth.protect();
 
-  const url = req.nextUrl
-  const searchParams = url.searchParams.toString()
-  let hostname = req.headers
+  const { nextUrl } = req;
+  const searchParams = nextUrl.searchParams.toString();
+  const hostname = req.headers.get("host");
 
-  const pathWithSearchParams = `${url.pathname}${
-    searchParams.length > 0 ? `?${searchParams}` : ''
-  }`
+  const pathWithSearchParams = `${nextUrl.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
 
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+  // Check for subdomain
+  const customSubDomain = hostname
+    ?.split(process.env.NEXT_PUBLIC_DOMAIN!)
+    .filter(Boolean)[0];
+
+  if (customSubDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    );
   }
-})
+
+  if (nextUrl.pathname === "/sign-in" || nextUrl.pathname === "/sign-up") {
+    return NextResponse.redirect(new URL("/agency/sign-in", req.url));
+  }
+
+  if (
+    nextUrl.pathname === "/" ||
+    (nextUrl.pathname === "/site" && nextUrl.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL("/site", req.url));
+  }
+
+  if (
+    nextUrl.pathname.startsWith("/agency") ||
+    nextUrl.pathname.startsWith("/subaccount")
+  ) {
+    return NextResponse.rewrite(new URL(pathWithSearchParams, req.url));
+  }
+});
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
-}
+};
